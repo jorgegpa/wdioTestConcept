c3058fd6f34f30d6288a32b3693691e2d9fbabb7
Jorge Gonzalez Pliego <46504211+jorgegpa@users.noreply.github.com>
2020-04-11 15:42:19 -0500

    add eslint config (#2)
    Co-authored-by: Jorge Gonzalez Pliego Arce <georgevx@MacBook-Pro-de-Jorge.local>

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -1,13 +1,11 @@
 import BaseTest from '../../test/baseTest'
 import DashboardPage from '../../pages/dashboard.page'
 
-
-  describe('Login Test', () => {
-     beforeEach(() => {
-         BaseTest.preCondition()
-     })
-      it('Succesfully Test Case', () => {
-          expect(DashboardPage.todayHeader).toBeDisplayed()
-      })
+describe('Login Test', () => {
+  beforeEach(() => {
+    BaseTest.preCondition()
   })
-
+  it('Succesfully Test Case', () => {
+    expect(DashboardPage.todayHeader).toBeDisplayed()
+  })
+})
