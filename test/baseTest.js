f10076620edfd9968ca2eaced6fd22ba72ea8e39
Jorge Gonzalez Pliego <46504211+jorgegpa@users.noreply.github.com>
2020-04-11 16:00:05 -0500

    Development (#3)
    * create actions file (#1)
    
    * create actions file
    
    * adding env and secrets
    
    * adding env and secrets
    
    * updating variables
    
    Co-authored-by: Jorge Gonzalez Pliego Arce <georgevx@MacBook-Pro-de-Jorge.local>
    
    * add eslint config (#2)
    
    Co-authored-by: Jorge Gonzalez Pliego Arce <georgevx@MacBook-Pro-de-Jorge.local>
    
    * cron scheduler
    
    * cron scheduler
    
    * cron scheduler
    
    * cron scheduler
    
    * cron scheduler
    
    * cron scheduler
    
    Co-authored-by: Jorge Gonzalez Pliego Arce <georgevx@MacBook-Pro-de-Jorge.local>

--- test/baseTest.js
+++ test/baseTest.js
@@ -1,15 +1,14 @@
 import LoginPage from '../pages/login.page'
 import {} from 'dotenv/config'
-class BaseTest{
-  
-    preCondition(){
-     const usrname = process.env.USERNAME
-     const passwd = process.env.PASSWORD
-     LoginPage.open(process.env.URL_HOST)
-     LoginPage.setUsername(usrname)
-     LoginPage.setPassword(passwd)
-     LoginPage.submit()
-   }
+class BaseTest {
+  preCondition () {
+    const usrname = process.env.TODOIST_USERNAME
+    const passwd = process.env.TODOIST_PASSWORD
+    LoginPage.open(process.env.URL_HOST)
+    LoginPage.setUsername(usrname)
+    LoginPage.setPassword(passwd)
+    LoginPage.submit()
+  }
 }
 
 export default new BaseTest()
